b81db07ce3e58f067d6a13dc4fabdf7423fb1796
Ariel Febrian <arielfebrian98@gmail.com>
2021-10-16 14:03:40 +0700

    update UI

--- src/components/Navbar/NavbarIcon/index.tsx
+++ src/components/Navbar/NavbarIcon/index.tsx
@@ -1,13 +1,18 @@
 import { MdHome, MdExplore, MdFavoriteBorder, MdChatBubble } from "react-icons/md";
+import ProfileIcon from "./ProfileIcon"
 
 export default function index() {
+
+    const navbarIconStyle = `m-36 p-5 flex flex-row items-center  justify-around w-64`
+
     return (
-        <div className="m-36 p-5 flex flex-row items-center  justify-around w-64" >
+        <div className={navbarIconStyle} >
 
             <MdHome size={30} cursor='pointer' />
             <MdChatBubble size={27} cursor='pointer' />
             <MdExplore size={27} cursor='pointer' />
             <MdFavoriteBorder size={27} cursor='pointer' />
+            <ProfileIcon />
         </div>
     )
 }
